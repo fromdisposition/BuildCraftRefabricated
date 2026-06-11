63a1a854e2ebdafdee3846bf4c1324a1644cad54
User <user@local>
2026-06-11 22:29:47 +0300

    Halve default rich oil sector coverage for desert and ocean.
    Reduce oilDesertRichChancePercent to 20% and oilOceanPatchChancePercent to 10% so rich 8x8 sectors are half as common.

--- src/main/java/buildcraft/energy/BCEnergyConfig.java
+++ src/main/java/buildcraft/energy/BCEnergyConfig.java
@@ -26,9 +26,9 @@ public class BCEnergyConfig {
    public static final BCCoreConfig.BooleanValue enableOilSprings = new BCCoreConfig.BooleanValue(true);
    public static final BCCoreConfig.BooleanValue enableWaterSpringGeneration = new BCCoreConfig.BooleanValue(true);
    /** Percent of 8×8-chunk desert sectors that upgrade to rich tier (wells + lakes) on top of normal baseline. */
-   public static final BCCoreConfig.IntValue oilDesertRichChancePercent = new BCCoreConfig.IntValue(40);
+   public static final BCCoreConfig.IntValue oilDesertRichChancePercent = new BCCoreConfig.IntValue(20);
    /** Percent of 8×8-chunk ocean sectors that upgrade to patch tier (wells + lakes) on top of normal baseline. */
-   public static final BCCoreConfig.IntValue oilOceanPatchChancePercent = new BCCoreConfig.IntValue(20);
+   public static final BCCoreConfig.IntValue oilOceanPatchChancePercent = new BCCoreConfig.IntValue(10);
    public static final BCCoreConfig.StringListValue excludedDimensions = new BCCoreConfig.StringListValue(List.of("minecraft:the_nether", "minecraft:the_end"));
    public static final BCCoreConfig.EnumValue<BCEnergyConfig.ListMode> dimensionListMode = new BCCoreConfig.EnumValue<>(BCEnergyConfig.ListMode.BLACKLIST);
 
